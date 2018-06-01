f1b99bacae70073d18f68a4f3ab6ac4b36bf1e7f
ibby730d <ibrar.mohammed@ffre.es>
2018-06-01 15:02:27 +0100

    Testing

---
+++ src/FirstTest2/Testing2.java
@@ -0,0 +1,18 @@
+package FirstTest2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testing2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "/home/ibrar/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://beta06.uaccount.uk/");
+		
+	}
+
+}
